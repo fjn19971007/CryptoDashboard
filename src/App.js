ea3ffd70881cc2c8200b61f5dc1c3c91cdf35031
Jianan Fang <fjn19971007@outlook.com>
2018-10-16 23:46:02 -0500

    add menu bar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 import styled from 'styled-components';
 
-const CustomElement = styled.div`
-  color: green;
-`
+const Logo = styled.div`
+  font-size: 1.5em;
+`;
+
+const ControlButton = styled.div`
+
+`;
+
+const AppLayout = styled.div`
+  padding: 40px;
+  display: grid;
+  grid-template-columns: 180px auto 100px 100px;
+`;
 
 class App extends Component {
   render() {
     return (
-      <CustomElement>Hi</CustomElement>
+      <AppLayout>
+        <Logo>
+          CryptoDashboard
+        </Logo>
+        <div/>
+        <ControlButton>
+          Dashboard
+        </ControlButton>
+        <ControlButton>
+          Settings
+        </ControlButton>
+      </AppLayout>
     );
   }
 }
