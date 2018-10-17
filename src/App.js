59251c5f487b2380c133346c0abd4bc71dcf391b
Jianan Fang <fjn19971007@outlook.com>
2018-10-17 00:55:52 -0500

    finish dynamic content

--- src/App.js
+++ src/App.js
@@ -1,35 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Logo = styled.div`
   font-size: 1.5em;
 `;
 
 const ControlButton = styled.div`
-
+  cursor: pointer;
+  ${props => props.active && css`
+    text-shadow: 0px 0px 60px #03ff03;
+  `}
 `;
 
 const AppLayout = styled.div`
   padding: 40px;
+`;
+
+const Bar = styled.div`
   display: grid;
   grid-template-columns: 180px auto 100px 100px;
 `;
 
+const Content = styled.div`
+
+`;
+
 class App extends Component {
+  state = {
+    page: 'dashboard'
+  };
+
+  displayingDashboard = () => this.state.page === 'dashboard';
+  displayingSettings = () => this.state.page === 'settings';
+
   render() {
     return (
       <AppLayout>
-        <Logo>
-          CryptoDashboard
-        </Logo>
-        <div/>
-        <ControlButton>
-          Dashboard
-        </ControlButton>
-        <ControlButton>
-          Settings
-        </ControlButton>
+        <Bar>
+          <Logo>
+            CryptoDashboard
+          </Logo>
+          <div/>
+          <ControlButton
+            active={this.displayingDashboard()}
+            onClick={() => this.setState({ page: 'dashboard' })}
+          >
+            Dashboard
+          </ControlButton>
+          <ControlButton
+            active={this.displayingSettings()}
+            onClick={() => this.setState({ page: 'settings' })}
+          >
+            Settings
+          </ControlButton>
+        </Bar>
+        <Content>
+          Hello
+        </Content>
       </AppLayout>
     );
   }
