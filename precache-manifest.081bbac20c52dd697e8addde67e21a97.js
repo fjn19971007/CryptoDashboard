df47d9c4e3cd084533c0901d750c7ff4fb1f1eae
Jianan Fang <fjn19971007@outlook.com>
2018-10-26 11:50:54 -0500

    Updates

---
+++ precache-manifest.081bbac20c52dd697e8addde67e21a97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd3672f93fed88a70bb5",
+    "url": "/CryptoDashboard/static/js/runtime~main.bd3672f9.js"
+  },
+  {
+    "revision": "56f8537ece72e835f239",
+    "url": "/CryptoDashboard/static/js/main.56f8537e.chunk.js"
+  },
+  {
+    "revision": "41380f8e5fe85e99714a",
+    "url": "/CryptoDashboard/static/js/1.41380f8e.chunk.js"
+  },
+  {
+    "revision": "56f8537ece72e835f239",
+    "url": "/CryptoDashboard/static/css/main.7a36df6c.chunk.css"
+  },
+  {
+    "revision": "1ceb5522df7add973bbb59c6254bc3ae",
+    "url": "/CryptoDashboard/index.html"
+  }
+];
